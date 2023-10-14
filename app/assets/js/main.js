fc1fc323aa64dd9890cd36b28e2135a0a43a179d
resilva316 <resilva316@csu.fullerton.edu>
2023-10-13 22:06:27 -0700

    Final Update?

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,14 +1,1 @@
-// YOUR CODE GOES HERE:
-// Get the elements by their ID
-// var popupLink = document.getElementById("popup-link");
-// var popupWindow = document.getElementById("popup-window");
-// var closeButton = document.getElementById("close-button");
-// // Show the pop-up window when the link is clicked
-// popupLink.addEventListener("click", function (event) {
-//     event.preventDefault();
-//     popupWindow.style.display = "block";
-// });
-// // Hide the pop-up window when the close button is clicked
-// closeButton.addEventListener("click", function () {
-//     popupWindow.style.display = "none";
-// }); 
+// YOUR CODE GOES HERE: 
